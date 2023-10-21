f516cdf5656d549435f89abb36bff01346fdc5b0
Dipakkkk <dr916139@gmail.com>
2023-10-21 16:38:45 +0530

    faq-first-done

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -15,14 +15,10 @@ function Icon({ id, open }) {
       fill="none"
       className={`${
         id === open ? "rotate-180" : ""
-      } h-5 w-5 transition-transform`}
+      } w-7 sm:h-9 h-7 sm:w-9 transition-transform`}
     >
       <path
-        d="M24.1088 22.4909C24.7613 23.1659 25.8413 23.1659 26.4938 22.5134C27.1687 21.8384 27.1687 20.7809 26.4938 20.1284L18.6863 12.2759C18.3713 11.9609 17.9438 11.7809 17.4938 11.7809C17.0438 11.7809 16.6163 11.9609 16.3013 12.2759L8.49378 20.1284C8.15628 20.4434 7.99878 20.8709 7.99878 21.2984C7.99878 21.7484 8.15628 22.1759 8.49378 22.5134C9.16878 23.1659 10.2263 23.1659 10.8788 22.4909L17.4938 15.8534L24.1088 22.4909Z"
-        fill="#3C278D"
-      />
-      <path
-        d="M24.1088 22.4909C24.7613 23.1659 25.8413 23.1659 26.4938 22.5134C27.1687 21.8384 27.1687 20.7809 26.4938 20.1284L18.6863 12.2759C18.3713 11.9609 17.9438 11.7809 17.4938 11.7809C17.0438 11.7809 16.6163 11.9609 16.3013 12.2759L8.49378 20.1284C8.15628 20.4434 7.99878 20.8709 7.99878 21.2984C7.99878 21.7484 8.15628 22.1759 8.49378 22.5134C9.16878 23.1659 10.2263 23.1659 10.8788 22.4909L17.4938 15.8534L24.1088 22.4909Z"
+        d="M10.8912 12.5091C10.2387 11.8341 9.15875 11.8341 8.50625 12.4866C7.83125 13.1616 7.83125 14.2191 8.50625 14.8716L16.3137 22.7241C16.6287 23.0391 17.0562 23.2191 17.5062 23.2191C17.9562 23.2191 18.3837 23.0391 18.6987 22.7241L26.5062 14.8716C26.8437 14.5566 27.0012 14.1291 27.0012 13.7016C27.0012 13.2516 26.8437 12.8241 26.5062 12.4866C25.8312 11.8341 24.7737 11.8341 24.1212 12.5091L17.5062 19.1466L10.8912 12.5091Z"
         fill="#3C278D"
       />
     </svg>
@@ -36,15 +32,23 @@ export function FaqSection() {
 
   return (
     <>
-      <div className="container mx-auto px-3 md:px-5 flex justify-center items-center">
-        <div className="max-w-[834px] ">
+      <div className="container mx-auto px-3 md:px-5 pt-[71px] pb-[126px]">
+        <h1 className="font-Lato text-light-blackE mb-4 font-bold text-5xl text-center">
+          FAQ
+        </h1>
+        <p className="font-Lato text-base text-light-black text-center mx-auto max-w-[788px] leading-[150%] pb-4  ">
+          Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget
+          consectetur mi. Vulputate sapien a a bibendum suspendisse quam. Hac
+          posuere eget nam diam velit urna adipiscing diam.
+        </p>
+        <div className="max-w-[834px] mx-auto">
           <Accordion
             className="flex flex-col justify-between px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden relative"
             open={open === 1}
             icon={<Icon id={1} open={open} />}
           >
             <AccordionHeader onClick={() => handleOpen(1)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[130%] md:leading-[150%]  ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
                 <span className="font-normal">1.</span> What is a cryptocurrency
                 token?
               </h2>
@@ -67,7 +71,7 @@ export function FaqSection() {
             icon={<Icon id={2} open={open} />}
           >
             <AccordionHeader onClick={() => handleOpen(2)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[130%] md:leading-[150%]  ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
                 <span className="font-normal">2.</span> How can I buy
                 cryptocurrency tokens?
               </h2>
@@ -89,7 +93,7 @@ export function FaqSection() {
             icon={<Icon id={3} open={open} />}
           >
             <AccordionHeader onClick={() => handleOpen(3)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[130%] md:leading-[150%]  ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
                 <span className="font-normal">3.</span> What is a token wallet,
                 why do I need one?
               </h2>
@@ -112,7 +116,7 @@ export function FaqSection() {
             icon={<Icon id={4} open={open} />}
           >
             <AccordionHeader onClick={() => handleOpen(4)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[130%] md:leading-[150%]  ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
                 <span className="font-normal">4.</span> Are cryptocurrency
                 tokens secure?
               </h2>
@@ -134,7 +138,7 @@ export function FaqSection() {
             icon={<Icon id={5} open={open} />}
           >
             <AccordionHeader onClick={() => handleOpen(5)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[130%] md:leading-[150%]  ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
                 <span className="font-normal">5.</span> What is a token wallet,
                 and why do I need one?
               </h2>
--- src/components/OurTeam1.jsx
+++ src/components/OurTeam1.jsx
@@ -12,14 +12,7 @@ const OurTeam1 = () => {
         ullamcorper egestas lectus risus ornare ullamcorper fames. Cursus libero
         nec facilisis
       </p>
-      <h1 className="font-Lato text-light-blackE mb-4 font-bold text-5xl text-center">
-        FAQ
-      </h1>
-      <p className="font-Lato text-xs text-light-black text-center mx-auto max-w-[788px] leading-[150%] pb-4  ">
-        Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget
-        consectetur mi. Vulputate sapien a a bibendum suspendisse quam. Hac
-        posuere eget nam diam velit urna adipiscing diam.
-      </p>
+      
       <div>
         <div className="TeamCard1">
           <img src="" alt="" />
