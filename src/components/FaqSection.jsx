b01048b40c0eb76035d4dff92787dee1110e7551
Dipakkkk <dr916139@gmail.com>
2023-10-23 09:59:31 +0530

    faq-one-done

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -43,12 +43,12 @@ export function FaqSection() {
         </p>
         <div className="max-w-[834px] mx-auto">
           <Accordion
-            className="flex flex-col justify-between px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden relative"
+            className="flex flex-col justify-between   px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden relative"
             open={open === 1}
             icon={<Icon id={1} open={open} />}
           >
-            <AccordionHeader onClick={() => handleOpen(1)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
+            <AccordionHeader onClick={() => handleOpen(1)} className="py-4 ">
+              <h2 className="font-Inter text-light-blue  text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start mb-0  ">
                 <span className="font-normal">1.</span> What is a cryptocurrency
                 token?
               </h2>
@@ -66,12 +66,12 @@ export function FaqSection() {
           </Accordion>
 
           <Accordion
-            className="flex flex-col justify-between px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
+            className="flex flex-col justify-between  px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
             open={open === 2}
             icon={<Icon id={2} open={open} />}
           >
-            <AccordionHeader onClick={() => handleOpen(2)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
+            <AccordionHeader onClick={() => handleOpen(2)} className="py-4 ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start mb-0  ">
                 <span className="font-normal">2.</span> How can I buy
                 cryptocurrency tokens?
               </h2>
@@ -88,12 +88,12 @@ export function FaqSection() {
             </AccordionBody>
           </Accordion>
           <Accordion
-            className="flex flex-col justify-between px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
+            className="flex flex-col justify-between  px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
             open={open === 3}
             icon={<Icon id={3} open={open} />}
           >
-            <AccordionHeader onClick={() => handleOpen(3)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
+            <AccordionHeader onClick={() => handleOpen(3)} className="py-4 ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start mb-0  ">
                 <span className="font-normal">3.</span> What is a token wallet,
                 why do I need one?
               </h2>
@@ -111,12 +111,12 @@ export function FaqSection() {
           </Accordion>
 
           <Accordion
-            className="flex flex-col justify-between px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
+            className="flex flex-col justify-between  px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
             open={open === 4}
             icon={<Icon id={4} open={open} />}
           >
-            <AccordionHeader onClick={() => handleOpen(4)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
+            <AccordionHeader onClick={() => handleOpen(4)} className="py-4 ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start mb-0  ">
                 <span className="font-normal">4.</span> Are cryptocurrency
                 tokens secure?
               </h2>
@@ -133,12 +133,12 @@ export function FaqSection() {
             </AccordionBody>
           </Accordion>
           <Accordion
-            className="flex flex-col justify-between px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
+            className="flex flex-col justify-between  px-4 md:px-6 border rounded-[12px] border-light-blue my-4 overflow-hidden"
             open={open === 5}
             icon={<Icon id={5} open={open} />}
           >
-            <AccordionHeader onClick={() => handleOpen(5)}>
-              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start  ">
+            <AccordionHeader onClick={() => handleOpen(5)} className="py-4 ">
+              <h2 className="font-Inter text-light-blue text-base md:text-xl font-semibold leading-[140%] md:leading-[150%] text-start mb-0  ">
                 <span className="font-normal">5.</span> What is a token wallet,
                 and why do I need one?
               </h2>
