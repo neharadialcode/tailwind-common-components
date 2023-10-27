84f6e814aa89a4bceab5ef9080e9552d92d16fd6
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-10-27 16:35:43 +0530

    video sectin complete

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import sample_video from "../assets/videos/sample_video.mp4";
+import thumbnail from "../assets/images/png/aboutus_col_img.png";
+import play from "../assets/images/png/play_button.png";
 
 const Video = () => {
+  const [isVideoPlaying, setVideoPlaying] = useState(false);
+
+  const handleVideoClick = () => {
+    if (!isVideoPlaying) {
+      setVideoPlaying(true);
+      const video = document.getElementById("videoElement");
+      if (video) {
+        video.play();
+      }
+    }
+  };
+
   return (
     <>
       <a
@@ -12,10 +26,30 @@ const Video = () => {
       </a>
       <div className="py-12">
         <div className="max-w-[1188px] mx-auto px-6 h-[680px]">
-          <div className="flex justify-center items-centre h-full">
-            <video controls class="w-full h-full">
-              <source src={sample_video} type="video/mp4" />
-            </video>
+          <div className="flex justify-center items-centre h-full ">
+            <div className="relative">
+              <div className="relative w-full h-full">
+                <video
+                  id="videoElement"
+                  onClick={handleVideoClick}
+                  controls
+                  className="w-full h-full"
+                  poster={thumbnail}
+                >
+                  <source src={sample_video} type="video/mp4" />
+                </video>
+                {!isVideoPlaying && (
+                  <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+                    <img
+                      src={play}
+                      alt="play button"
+                      className="w-16 h-16 object-contain cursor-pointer"
+                      onClick={handleVideoClick}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
