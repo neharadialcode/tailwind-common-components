29afe95d232941f657bc558857f59c295f3c1c30
sachin-niyana <sachinjakhar100@gmail.com>
2023-10-28 13:01:19 +0530

    complet road map section

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const RoadMap = () => {
   return (
-    <div className="bg-white">
-      <div className="container xl:w-[1140px] 2xl:w-[1320px] mx-auto px-3 sm:px-5 xl:px-0">
+    <div className="bg-white ">
+      <div className="container xl:w-[1140px] 2xl:w-[1320px] mx-auto px-3 sm:px-5 xl:px-0 relative pb-24">
         <div className="flex justify-center">
-          <h3 className=" font-Inter font-semibold text-lg text-black">
+          <h3 className=" font-Inter font-semibold md:text-lg text-3md text-black">
             Road Map
           </h3>
         </div>
@@ -17,27 +17,69 @@ const RoadMap = () => {
             fames. Cursus libero nec facilisis
           </p>
         </div>
-        <div className="flex mt-20">
-          <div className=" w-2/4 relative">
-            <div className="flex items-center  absolute top-[50px] right-0">
-              <div className="w-[70px] h-[70px] rounded-[50%] border-[2px] border-black flex justify-center items-center">
-                <h3 className="text-black font-Inter font-normal text-3md">
+        <div className="bg-black w-[2px] h-[75%] absolute top-[10%] md:left-[50%] left-[8%] mt-16 md:translate-x-[-50%]">
+          <div className="md:hidden block">
+            <div className="flex items-center absolute top-[3%] left-[-25px] ">
+              <div className="w-[50px] h-[50px]  bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
                   1
                 </h3>
               </div>
-              <div className="w-[85px] h-[2px] bg-black"></div>
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
             </div>
           </div>
-          <div className="w-[2px] h-[320px] bg-black "></div>
-          <div className="w-2/4">
-            <div className="max-w-[430px] w-full ps-10">
+          <div className="md:hidden block">
+            <div className="flex items-center absolute top-[29%] left-[-25px] ">
+              <div className="w-[50px] h-[50px]  bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
+                  2
+                </h3>
+              </div>
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+            </div>
+          </div>
+          <div className="md:hidden block">
+            <div className="flex items-center absolute bottom-[41%] left-[-25px] ">
+              <div className="w-[50px] h-[50px]  bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
+                  3
+                </h3>
+              </div>
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+            </div>
+          </div>
+          <div className="md:hidden block">
+            <div className="flex items-center absolute bottom-[16%] left-[-25px] ">
+              <div className="w-[50px] h-[50px]  bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
+                  4
+                </h3>
+              </div>
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex mt-20">
+          <div className=" md:w-2/4 md:block hidden relative">
+            <div className="flex items-center absolute md:top-[50px] top-0 md:right-0 ">
+              <div className="w-[70px] h-[70px]  bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
+                  1
+                </h3>
+              </div>
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+            </div>
+          </div>
+          {/* <div className="w-[2px] h-[320px] bg-black "></div> */}
+          <div className="md:w-2/4 w-full">
+            <div className=" md:ps-10 ps-16">
               <div className="flex justify-start">
-                <h4 className="text-black font-Inter font-normal text-2md leading-normal">
+                <h4 className="text-black font-Inter max-w-[430px] font-normal md:text-2md text-md leading-normal">
                   Inception and prepration
                 </h4>
               </div>
               <div className="flex justify-start">
-                <p className="font-normal text-dark-gray font-Inter text-sm leading-[150%]">
+                <p className="font-normal max-w-[430px] text-dark-gray font-Inter text-sm leading-[150%]">
                   Lorem ipsum dolor sit amet consectetur. Velit pulvinar libero
                   porttitor quam sed aliquet. Sodales vulputate elementum arcu
                   donec. Feugiat nibh ullamcorper egestas lectus risus ornare
@@ -49,17 +91,17 @@ const RoadMap = () => {
             </div>
           </div>
         </div>
-        <div className="flex ">
-          <div className="w-2/4">
-            <div className="flex justify-end">
-              <div className="max-w-[430px] w-full pe-10">
-                <div className=" flex justify-end">
-                  <h4 className="text-black font-Inter font-normal text-2md leading-normal">
+        <div className="flex md:flex-row flex-row-reverse ">
+          <div className="md:w-2/4 w-full">
+            <div className="flex md:justify-end justify-start">
+              <div className=" md:pe-10 md:ps-0 ps-16">
+                <div className=" flex md:justify-end justify-start mt-20">
+                  <h4 className="text-black font-Inter max-w-[430px] font-normal md:text-2md text-md leading-normal">
                     Inception and prepration
                   </h4>
                 </div>
-                <div className="flex justify-end text-right">
-                  <p className="font-normal text-dark-gray font-Inter text-sm leading-[150%]">
+                <div className="flex md:text-right text-left">
+                  <p className="font-normal max-w-[430px] text-dark-gray font-Inter text-sm leading-[150%]">
                     Lorem ipsum dolor sit amet consectetur. Velit pulvinar
                     libero porttitor quam sed aliquet. Sodales vulputate
                     elementum arcu donec. Feugiat nibh ullamcorper egestas
@@ -71,13 +113,79 @@ const RoadMap = () => {
               </div>
             </div>
           </div>
-          <div className="w-[2px] h-[320px] bg-black "></div>
-          <div className=" w-2/4 relative">
-            <div className="flex items-center  absolute top-[50px] left-0">
-              <div className="w-[85px] h-[2px] bg-black"></div>
-              <div className="w-[70px] h-[70px] rounded-[50%] border-[2px] border-black flex justify-center items-center">
-                <h3 className="text-black font-Inter font-normal text-3md">
+          {/* <div className="w-[2px] h-[320px] bg-black "></div> */}
+          <div className=" md:w-2/4 md:block hidden relative">
+            <div className="flex items-center  absolute md:top-[120px] top-[80px] left-0">
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+              <div className="sm:w-[70px] w-[50px] sm:h-[70px] h-[50px] bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
                   2
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex mt-20">
+          <div className=" md:w-2/4 md:block hidden relative">
+            <div className="flex items-center absolute md:top-[50px] top-0 md:right-0">
+              <div className="sm:w-[70px] w-[50px] sm:h-[70px] h-[50px] bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
+                  3
+                </h3>
+              </div>
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+            </div>
+          </div>
+          {/* <div className="w-[2px] h-[320px] bg-black "></div> */}
+          <div className="md:w-2/4 w-full">
+            <div className=" md:ps-10 ps-16">
+              <div className="flex justify-start">
+                <h4 className="text-black font-Inter max-w-[430px] font-normal md:text-2md text-md leading-normal">
+                  Inception and prepration
+                </h4>
+              </div>
+              <div className="flex justify-start">
+                <p className="font-normal max-w-[430px] text-dark-gray font-Inter text-sm leading-[150%]">
+                  Lorem ipsum dolor sit amet consectetur. Velit pulvinar libero
+                  porttitor quam sed aliquet. Sodales vulputate elementum arcu
+                  donec. Feugiat nibh ullamcorper egestas lectus risus ornare
+                  ullamcorper fames. Cursus libero nec facilisis risus malesuada
+                  arcu cum. Gravida massa scelerisque sociis nulla diam nisl
+                  ligula.bh
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex md:flex-row flex-row-reverse ">
+          <div className="md:w-2/4 w-full">
+            <div className="flex md:justify-end justify-start">
+              <div className=" md:pe-10 md:ps-0 ps-16">
+                <div className=" flex md:justify-end justify-start mt-20">
+                  <h4 className="text-black font-Inter max-w-[430px] font-normal md:text-2md text-md leading-normal">
+                    Inception and prepration
+                  </h4>
+                </div>
+                <div className="flex md:text-right text-left">
+                  <p className="font-normal max-w-[430px] text-dark-gray font-Inter text-sm leading-[150%]">
+                    Lorem ipsum dolor sit amet consectetur. Velit pulvinar
+                    libero porttitor quam sed aliquet. Sodales vulputate
+                    elementum arcu donec. Feugiat nibh ullamcorper egestas
+                    lectus risus ornare ullamcorper fames. Cursus libero nec
+                    facilisis risus malesuada arcu cum. Gravida massa
+                    scelerisque sociis nulla diam nisl ligula.bh
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div className="w-[2px] h-[320px] bg-black "></div> */}
+          <div className=" md:w-2/4 md:block hidden relative">
+            <div className="flex items-center  absolute md:top-[120px] top-[80px] left-0">
+              <div className="md:block hidden w-[85px] h-[2px] bg-black"></div>
+              <div className="sm:w-[70px] w-[50px] sm:h-[70px] h-[50px] bg-white rounded-[50%] border-[2px] border-black flex justify-center items-center">
+                <h3 className="text-black font-Inter font-normal md:text-3md text-2md">
+                  4
                 </h3>
               </div>
             </div>
