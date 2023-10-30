7852b9796c8d2eeb4579d3a4895e065d04245597
anilkumar70820 <ak9565191@gmail.com>
2023-10-30 16:30:06 +0530

    roadmap align in center

--- src/components/RoadmapTwo.jsx
+++ src/components/RoadmapTwo.jsx
@@ -16,7 +16,7 @@ const RoadmapTwo = () => {
           </p>
         </div>
         <div className="relative flex lg:flex-col sm:flex-row flex-col sm:gap-[69px] lg:items-start 2xl:items-center sm:justify-around items-center">
-          <div className="flex gap-16 lg:gap-[100px] xl:gap-[158px] xl:ms-[182px] 2xl:ms-[350px] lg:ms-40  lg:flex-row flex-col">
+          <div className="flex gap-16 lg:gap-[80px] xl:gap-[130px] xl:ms-[190px] 2xl:ms-[175px] lg:ms-[145px]  lg:flex-row flex-col">
             <div className="w-[216px] h-[112px] sm:ps-[10px] ps-12 relative roadmap_cards rounded-xl cursor-pointer">
             <div className="w-[1.413px] h-[120px] bg-[#C5C2C2] absolute left-[-17px] top-[50%] sm:hidden"></div>
               <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:bottom-[-63%] lg:left-[50%] lg:translate-x-[-50%] md:right-[-50%] sm:right-[-35.6%] sm:bottom-[8%] sm:translate-y-[-50%] right-[100%] bottom-[5%] translate-y-[-50%]">
@@ -54,8 +54,8 @@ const RoadmapTwo = () => {
               </p>
             </div>
           </div>
-          <div className="hidden sm:block absolute lg:top-[51%] z-[-1] lg:left-[10%] lg:w-[81%] 2xl:w-[70%] 2xl:left-[21%] w-[1.413px] lg:h-[1.413px] sm:h-[68%] h-[92%] sm:top-[57%] translate-y-[-50%] sm:left-[50%] top-[50%] left-20 bg-[#C5C2C2]"></div>
-          <div className="flex lg:mt-0 sm:mt-[200px] mt-16 gap-16 lg:gap-[100px] xl:gap-[158px] lg:flex-row flex-col items-center">
+          <div className="hidden sm:block absolute lg:top-[51%] z-[-1] lg:left-[12%] lg:w-[75%] 2xl:w-[68%] 2xl:left-[16%] w-[1.413px] lg:h-[1.413px] sm:h-[68%] h-[92%] sm:top-[57%] translate-y-[-50%] sm:left-[50%] top-[50%] left-20 bg-[#C5C2C2]"></div>
+          <div className="flex lg:mt-0 sm:mt-[200px] mt-16 gap-16 lg:gap-[80px] xl:gap-[130px] lg:flex-row flex-col items-center xl:ms-8 2xl:ms-0 2xl:me-[175px]">
             <div className="w-[216px] h-[112px] roadmap_cards sm:ps-[10px] ps-12 relative cursor-pointer">
               <div className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></div>
               <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:top-[-30%] lg:left-[50%] lg:translate-x-[-50%] md:left-[-50%] sm:left-[-35%] sm:top-[50%] translate-y-[-50%] right-[100%] top-[50%]">
