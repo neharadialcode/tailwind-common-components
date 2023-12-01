2545df153157d583afdce344841965f6ff4373bf
sachin-niyana <sachinjakhar100@gmail.com>
2023-12-01 10:41:01 +0530

    take pull from newheader

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -22,10 +22,10 @@ const OurTeam = () => {
           {card.map((data) => {
             return (
               <div className=" px-3 lg:w-1/4 sm:w-2/4 group">
-                <div className="max-w-[345px] px-3 bg-card-bg lg:mb-0 mb-6 pt-6 pb-6 sm:py-8 rounded-xl hover:scale-105 duration-300 transition-all">
+                <div className="max-w-345px px-3 bg-card-bg lg:mb-0 mb-6 p-y6 sm:py-8 rounded-xl hover:scale-105 duration-300 transition-all">
                   <div className="flex justify-center">
                     <img
-                      className="xl:w-[120px] lg:w-[90px] xl:h-[120px] lg:h-[90px] md:w-[120px] md:h-[120px] w-[80px] h-[80px] min-w-[80px] min-h-[80px] duration-300 transition-all"
+                      className="xl:w-[120px] xl:h-[120px] lg:w-[100px] lg:h-[100px] md:w-[120px] md:h-[120px] w-[100px] h-[100px] duration-300 transition-all"
                       src={data.img}
                       alt="men-1"
                     />
--- src/components/RoadmapTwo.jsx
+++ src/components/RoadmapTwo.jsx
@@ -16,10 +16,10 @@ const RoadmapTwo = () => {
           </p>
         </div>
         <div className="sm:relative flex lg:flex-col sm:flex-row flex-col sm:gap-[69px] lg:items-start 2xl:items-center sm:justify-around items-center">
-          <div className="flex gap-10 sm:gap-32 lg:gap-[80px] xl:gap-[130px] xl:ms-[190px] 2xl:ms-[175px] lg:ms-[145px]  lg:flex-row flex-col">
+          <div className="flex gap-10 sm:gap-32 lg:gap-[80px] xl:gap-[130px] xl:ms-[190px] 2xl:ms-[175px] lg:ms-[145px] lg:flex-row flex-col">
             <div className="relative sm:ps-0 ps-12">
               <div className="w-[216px] h-[118px] roadmap_cards rounded-xl border-transparent hover:border hover:border-[#3C278D38]">
-                <div className="w-[1.413px] h-[120px] bg-[#C5C2C2] absolute left-[-17px] top-[50%] sm:hidden"></div>
+                <span className="w-[1.413px] h-[120px] bg-[#C5C2C2] absolute left-[-17px] top-[50%] sm:hidden"></span>
                 <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:bottom-[-60%] lg:left-[50%] lg:translate-x-[-50%] md:right-[-50%] sm:right-[-35.6%] sm:bottom-[8%] sm:translate-y-[-50%] right-[100%] bottom-[20%] translate-y-[-50%]">
                   <div className=" w-5 h-5 rounded-full bg-[#3C278D]"></div>
                 </div>
@@ -33,11 +33,11 @@ const RoadmapTwo = () => {
             </div>
             <div className="relative sm:ps-0 ps-12">
               <div className="w-[216px] h-[118px] roadmap_cards rounded-xl border-transparent hover:border hover:border-[#3C278D38]">
-                <div className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></div>
+                <span className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></span>
                 <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:bottom-[-60%] lg:left-[50%] lg:translate-x-[-50%] md:right-[-50%] sm:right-[-35.6%] sm:bottom-[8%] translate-y-[-50%] right-[100%] bottom-[20%]">
-                  <div className=" w-5 h-5 rounded-full bg-[#3C278D]"></div>
+                  <span className=" w-5 h-5 rounded-full bg-[#3C278D]"></span>
                 </div>
-                <h2 className="roadmap_heading duration-300 rounded-t-xl  font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
+                <h2 className="roadmap_heading duration-300 rounded-t-xl font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
                   Lorem ipsum{" "}
                 </h2>
                 <p className="roadmap_para duration-300 rounded-b-xl font-Inter text-sm font-normal text-black opacity-70 text-center">
@@ -47,11 +47,11 @@ const RoadmapTwo = () => {
             </div>
             <div className="relative sm:ps-0 ps-12">
               <div className="w-[216px] h-[118px] roadmap_cards rounded-xl border-transparent hover:border hover:border-[#3C278D38]">
-                <div className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></div>
+                <span className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></span>
                 <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:bottom-[-60%] lg:left-[50%] lg:translate-x-[-50%] md:right-[-50%] sm:right-[-35.6%] sm:bottom-[8%] translate-y-[-50%]  right-[100%] bottom-[20%]">
-                  <div className=" w-5 h-5 rounded-full bg-[#3C278D]"></div>
+                  <span className=" w-5 h-5 rounded-full bg-[#3C278D]"></span>
                 </div>
-                <h2 className="roadmap_heading duration-300 rounded-t-xl  font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
+                <h2 className="roadmap_heading duration-300 rounded-t-xl font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
                   Lorem ipsum{" "}
                 </h2>
                 <p className="roadmap_para duration-300 rounded-b-xl font-Inter text-sm font-normal text-black opacity-70 text-center">
@@ -64,9 +64,9 @@ const RoadmapTwo = () => {
           <div className="flex lg:mt-0 sm:mt-[265px] mt-16 gap-10 sm:gap-32 lg:gap-[80px] xl:gap-[130px] lg:flex-row flex-col items-center xl:ms-8 2xl:ms-0 2xl:me-[175px]">
             <div className="relative sm:ps-0 ps-12">
               <div className="w-[216px] h-[118px] roadmap_cards rounded-xl border-transparent hover:border hover:border-[#3C278D38]">
-                <div className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></div>
+                <span className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></span>
                 <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:top-[-27%] lg:left-[50%] lg:translate-x-[-50%] md:left-[-50%] sm:left-[-35%] sm:top-[50%] translate-y-[-50%] right-[100%] top-[50%]">
-                  <div className=" w-5 h-5 rounded-full bg-[#3C278D]"></div>
+                  <span className=" w-5 h-5 rounded-full bg-[#3C278D]"></span>
                 </div>
                 <h2 className="roadmap_heading duration-300 rounded-t-xl font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
                   Lorem ipsum{" "}
@@ -78,11 +78,11 @@ const RoadmapTwo = () => {
             </div>
             <div className="relative sm:ps-0 ps-12">
               <div className="w-[216px] h-[118px] roadmap_cards rounded-xl border-transparent hover:border hover:border-[#3C278D38]">
-                <div className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></div>
+                <span className="w-[1.413px] h-[185px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></span>
                 <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:top-[-27%] lg:left-[50%] lg:translate-x-[-50%] md:left-[-50%] sm:left-[-35%] sm:top-[50%] translate-y-[-50%] right-[100%] top-[50%]">
-                  <div className=" w-5 h-5 rounded-full bg-[#3C278D]"></div>
+                  <span className=" w-5 h-5 rounded-full bg-[#3C278D]"></span>
                 </div>
-                <h2 className="roadmap_heading duration-300 rounded-t-xl  font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
+                <h2 className="roadmap_heading duration-300 rounded-t-xl font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
                   Lorem ipsum{" "}
                 </h2>
                 <p className="roadmap_para duration-300 rounded-b-xl font-Inter text-sm font-normal text-black opacity-70 text-center">
@@ -92,11 +92,11 @@ const RoadmapTwo = () => {
             </div>
             <div className="relative sm:ps-0 ps-12">
               <div className="w-[216px] h-[118px] roadmap_cards rounded-xl border-transparent hover:border hover:border-[#3C278D38]">
-                <div className="w-[1.413px] h-[40px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></div>
+                <span className="w-[1.413px] h-[40px] bg-[#C5C2C2] absolute left-[-17px] top-0 sm:hidden"></span>
                 <div className="w-[34px] h-[34px] rounded-full border border-[#3C278D] flex justify-center items-center absolute lg:top-[-27%] lg:left-[50%] lg:translate-x-[-50%] md:left-[-50%] sm:left-[-35%] sm:top-[50%] translate-y-[-50%]  right-[100%] top-[50%]">
-                  <div className=" w-5 h-5 rounded-full bg-[#3C278D]"></div>
+                  <span className=" w-5 h-5 rounded-full bg-[#3C278D]"></span>
                 </div>
-                <h2 className="roadmap_heading duration-300 rounded-t-xl  font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
+                <h2 className="roadmap_heading duration-300 rounded-t-xl font-Inter pb-2 text-[#1E1E1E] font-semibold text-md text-center">
                   Lorem ipsum{" "}
                 </h2>
                 <p className="roadmap_para duration-300 rounded-b-xl font-Inter text-sm font-normal text-black opacity-70 text-center">
